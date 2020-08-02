386499af7fe9b191cc25254d2410c47237910587
LironBayda <34834883+LironBayda@users.noreply.github.com>
2020-08-02 21:30:59 +0300

    Add files via upload

---
+++ guest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+   public class Guest
+    {
+        int _id;
+        string _name;
+
+        public Guest(int id, string name)
+        {
+            _id = id;
+            _name = name;
+        }
+        public int GetId()
+        {
+            return _id;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public override string ToString()
+        {
+            return base.ToString()+ $" _id: {_id} _name:{_name}";
+        }
+    }
+}
